6bfea7b79521bb5bb3db76f039daf8ed1c6a3f99
Kamil <uhkamil@gmail.com>
2026-03-10 02:33:36 +0100

    Created solutions for task 2, labs 01 (previous were 1a and 1b). Although the code seems correct, the tester class doesn't handle it properly

--- Labs/lab01/dsaa/lab01/Document.java
+++ Labs/lab01/dsaa/lab01/Document.java
@@ -3,15 +3,39 @@ package dsaa.lab01;
 import java.util.Scanner;
 
 public class Document {
-	public static void loadDocument(String name, Scanner scan) {
-		//TODO
-		
-	}
-	
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	public static boolean correctLink(String link) {
-		// TODO
-		return true;
-	}
+    public static void loadDocument(String name, Scanner scan) {
+//        boolean firstLine = false;
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+//            System.out.println(line);
+            if (line.trim().equals("eod")) break;
+            String[] words = line.trim().split("\\s+");
+            // for word in words, check for link correctness, then if it's correct, print it
+            for (String word : words) {
+                if (word.toLowerCase().startsWith("link=")) {
+                    String id = word.substring(5);
+                    if (correctLink(id)) {
+//                        if (firstLine) System.out.print("\n");
+//                        firstLine = true;
+//                        System.out.print(id.toLowerCase());
+                        System.out.println(id.toLowerCase());
+                    }
+                }
+            }
+        }
+    }
+
+    // accepted only small letters, capitalic letter, digits and '_' (but not on the begin)
+    public static boolean correctLink(String link) {
+        if (link.isEmpty()) return false;
+        if (!Character.isLetter(link.charAt(0))) return false;
+        for (int i = 1; i < link.length(); i++) {
+            char c = link.charAt(i);
+            if (!Character.isLetterOrDigit(c)) {
+                if (c != '_') return false;
+            }
+        }
+        return true;
+    }
 
 }
--- Labs/lab01/dsaa/lab01/Execution.java
+++ Labs/lab01/dsaa/lab01/Execution.java
@@ -1,13 +1,18 @@
 package dsaa.lab01;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Execution {
     // Solely for testing how the subparts of my algorithms work
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 //        Drawer.drawLine(3, '.');
 //        Drawer.drawLine(1, 'X');
 //        Drawer.drawLine(3, '.');
 //        Drawer.drawPyramid(4);
 //        Drawer.drawPyramid(5);
 //        Drawer.drawChristmassTree(4);
+        Document.loadDocument("testdoc", new Scanner(new File("./testdoc.txt")));
     }
 }
