82856aa7da33f217f9cda5a47c1cc79ef8eaa19f
Kamil <uhkamil@gmail.com>
2026-05-07 15:42:54 +0200

    completed labs 06

--- Labs/dsaa/lab06/Document.java
+++ Labs/dsaa/lab06/Document.java
@@ -3,83 +3,275 @@ package dsaa.lab06;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class Document{
-	public String name;
-	public TwoWayCycledOrderedListWithSentinel<Link> link;
-	public Document(String name, Scanner scan) {
-		this.name=name.toLowerCase();
-		link=new TwoWayCycledOrderedListWithSentinel<Link>();
-		load(scan);
-	}
-	public void load(Scanner scan) {
-		//TODO
-	}
-	
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	
+public class Document {
+    public String name;
+    public TwoWayCycledOrderedListWithSentinel<Link> link;
 
-	public static boolean isCorrectId(String id) {
-		//TODO
-		return false;
-	}
+    public Document(String name, Scanner scan) {
+        this.name = name.toLowerCase();
+        link = new TwoWayCycledOrderedListWithSentinel<>();
+        load(scan);
+    }
 
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	public static Link createLink(String link) {
-		//TODO
-		return null;
-	}
+    public void load(Scanner scan) {
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
 
-	@Override
-	public String toString() {
-		String retStr="Document: "+name;
-		//TODO
-		return retStr;
-	}
+            if (line.trim().equalsIgnoreCase("eod")) {
+                break;
+            }
 
-	public String toStringReverse() {
-		String retStr="Document: "+name;
-		ListIterator<Link> iter=link.listIterator();
-		while(iter.hasNext())
-			iter.next();
-		//TODO
-		while(iter.hasPrevious()){
-		}
-		return retStr;
-	}
-	public int[] getWeights() {
-		//TODO
-		return null;
-	}
+            String[] words = line.trim().split("\\s+");
 
-	public static void showArray(int[] arr) {
-		// TODO
-	}
+            for (String word : words) {
+                if (word.toLowerCase().startsWith("link=")) {
+                    String linkStr = word.substring(5);
+                    Link newLink = createLink(linkStr);
 
-	void bubbleSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+                    if (newLink != null) {
+                        link.add(newLink);
+                    }
+                }
+            }
+        }
+    }
 
-	public void insertSort(int[] arr) {
-		showArray(arr);
-		//TODO
-		
-	}
-	public void selectSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
-	
+    // accepted only small letters, capital letter, digits nad '_' (but not at the beginning)
+    public static boolean isCorrectId(String id) {
+        return id.matches("^[a-zA-Z][a-zA-Z0-9_]*$");
+    }
 
-	public void iterativeMergeSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+    // accepted only small letters, capital letter, digits nad '_' (but not at the beginning)
+    public static Link createLink(String link) {
+        int openBracket = link.indexOf('(');
+        int closeBracket = link.indexOf(')');
 
-	public void radixSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+        if (link.contains("(") && link.contains(")")) {
+
+            String ref = link.substring(0, openBracket).toLowerCase();
+            if (!isCorrectId(ref)) return null;
+
+            String weightStr = link.substring(openBracket + 1, closeBracket);
+            try {
+                int weight = Integer.parseInt(weightStr);
+                if (weight <= 0) return null;
+                return new Link(ref, weight);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        } else {
+            String ref = link.toLowerCase();
+            if (isCorrectId(ref)) return new Link(ref);
+            else return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Document: ").append(name).append("\n");
+        int count = 0;
+
+        for (Link l : link) {
+            sb.append(l.toString());
+            count++;
+            if (count % 10 == 0) {
+                sb.append("\n");
+            } else {
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public String toStringReverse() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Document: ").append(name).append("\n");
+
+        ListIterator<Link> iter = link.listIterator();
+        while (iter.hasNext()) {
+            iter.next();
+        }
+
+        int count = 0;
+        while (iter.hasPrevious()) {
+            sb.append(iter.previous().toString());
+            count++;
+            if (count % 10 == 0) {
+                sb.append("\n");
+            } else {
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public int[] getWeights() {
+        int[] weights = new int[link.size()];
+        int i = 0;
+        for (Link currentLink : link) {
+            weights[i] = currentLink.weight;
+            i++;
+        }
+        return weights;
+    }
+
+    public static void showArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    void bubbleSort(int[] arr) {
+        showArray(arr);
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+            showArray(arr);
+        }
+    }
+
+    public void insertSort(int[] arr) {
+        showArray(arr);
+        int n = arr.length;
+        for (int i = n - 1; i > 0; i--) {
+            int key = arr[i - 1];
+            int j = i;
+            while (j < n && arr[j] < key) {
+                arr[j - 1] = arr[j];
+                j++;
+            }
+            arr[j - 1] = key;
+            showArray(arr);
+        }
+    }
+
+    public void selectSort(int[] arr) {
+        showArray(arr);
+        int n = arr.length;
+
+        for (int i = n - 1; i > 0; i--) {
+            int maxIndex = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+            showArray(arr);
+        }
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left];
+        int i = left;
+        int j = mid;
+        int k = 0;
+
+        while (i < mid && j < right) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i += 1;
+            } else {
+                temp[k] = arr[j];
+                j += 1;
+            }
+            k += 1;
+        }
+        while (i < mid) {
+            temp[k] = arr[i];
+            i += 1;
+            k += 1;
+        }
+        while (j < right) {
+            temp[k] = arr[j];
+            j += 1;
+            k += 1;
+        }
+        System.arraycopy(temp, 0, arr, left, temp.length);
+    }
+
+    public static void iterativeMergeSort(int[] arr) {
+        showArray(arr);
+        int size = 1;
+        while (size < arr.length) {
+            int left = 0;
+            while (left < arr.length - 1) {
+                int mid = Math.min(left + size, arr.length);
+                int right = Math.min(left + 2 * size, arr.length);
+                merge(arr, left, mid, right);
+                left += 2 * size;
+            }
+            showArray(arr);
+            size *= 2;
+        }
+    }
+
+    public static int getDigit(int number, int position) {
+        int i = 0;
+        int digit = 0;
+        while (i < position) {
+            digit = number % 10;
+            number /= 10;
+            i++;
+        }
+        return Math.abs(digit);
+    }
+
+    public static void radixSort(int[] arr) {
+        showArray(arr);
+        int max = 0;
+        int sortAmount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (Math.abs(arr[i]) > max) max = Math.abs(arr[i]);
+        }
+        while (max >= 1) {
+            max /= 10;
+            sortAmount += 1;
+        }
+        // output test
+        if (sortAmount < 3) {
+            sortAmount = 3;
+        }
+
+        for (int i = 1; i <= sortAmount; i++) {
+            int[] occurrences = new int[10];
+            for (int j = 0; j < arr.length; j++) {
+                int digit = getDigit(arr[j], i);
+                occurrences[digit]++;
+            }
+
+            // prefix sums
+            for (int k = 1; k <= 9; k++) {
+                occurrences[k] = occurrences[k - 1] + occurrences[k];
+            }
+
+            int[] output = new int[arr.length];
+
+            for (int j = arr.length - 1; j >= 0; j--) {
+                int digit = getDigit(arr[j], i);
+                int targetIndex = occurrences[digit] - 1;
+                output[targetIndex] = arr[j];
+                occurrences[digit]--;
+            }
+
+            System.arraycopy(output, 0, arr, 0, arr.length);
+            showArray(arr);
+        }
 
 
+    }
 }
